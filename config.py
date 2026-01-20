8582e7356d97d689a0d6f5942871ab5d9b911a6c
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2026-01-20 14:02:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "736532225-w") 
+    API_ID    = os.environ.get("API_ID", "20919286")
+    API_HASH  = os.environ.get("API_HASH", "57b85f72104db3f08f9795b0410eb556")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8417911153:AAFE1k80yY6nhFnwFQ9hy3d-KeVaXhDBHHQ") 
 
     # database config
-    DB_NAME = os.environ.get("DB_NAME","Yato")     
-    DB_URL  = os.environ.get("DB_URL","mongodb")
+    DB_NAME = os.environ.get("DB_NAME","cluster0")     
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://achu:achu@cluster0.5kmrm1f.mongodb.net/?appName=Cluster0")
     PORT = os.environ.get("PORT", "8080")
  
     # other configs
@@ -19,8 +19,8 @@ class Config(object):
     START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/29a3acbbab9de5f45a5fe.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6497757690').split()]
     FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', 'codeflix_bots').split(',')
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001868871195"))
-    DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1001868871195"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003454929996"))
+    DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1003454929996"))
     
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
